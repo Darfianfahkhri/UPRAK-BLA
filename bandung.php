db9671ca6ca93ae046589cd6d2c78db1f4e5c58a
Darfian Fakhri Usmawan <148190617+Darfianfahkhri@users.noreply.github.com>
2023-12-08 13:45:23 +0700

    Add files via upload

---
+++ bandung.php
@@ -0,0 +1,311 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- my css -->
+    <link rel="stylesheet" href="style.css">
+
+     <title>Bandung Lautan API</title>
+   </head>
+   <body id="Sejarah">
+   <style>
+  @import url('https://fonts.googleapis.com/css2?family=Rubik+Bubbles&display=swap');
+  *{
+    font-family: "Rubik Bubbles";
+  }
+</style>
+
+
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light sm fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Darfian Fakhri Usmawan</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav ms-auto">
+            <a class="nav-link active" aria-current="page" href="#Sejarah">Sejarah</a>
+            <a class="nav-link active"a href="#Tokoh">Tokoh</a>
+            <a class="nav-link active"a href="#Dokumentasi">Dokumentasi</a>
+            <a class="nav-link active"a href="#Hikmah">Hikmah</a>
+            <a class="nav-link active"a href="#Pesan">Pesan</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <!-- navbar akhir -->
+    
+
+
+
+
+    <!-- jumbotron -->
+  <section class="jumbotron text-center fw-bold">
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+  <h1 class="display-4">BANDUNG LAUTAN API</h1>
+  <br>
+  <p class="lead">Peristiwa ini menjadi saksi perjuangan rakyat Indonesia melawan Sekutu. Seperti namanya, Bandung Lautan Api adalah kebakaran besar yang melanda kota Bandung. Tragedi Bandung Lautan Api dimulai dengan kedatangan pasukan Inggris ke Bandung. Kemudian, tentara Sekutu memasuki wilayah Bandung pada Oktober 1945. Pada saat itu, pemuda Bandung sibuk merebut senjata pasukan Jepang. Pasukan yang berafiliasi dengan NICA (Netherlands Indies Civil Administration) mengeluarkan ultimatum kepada rakyat Bandung.  Isi ultimatum tersebut meminta agar Bandung dikosongkan dan senjata yang diambil dari pasukan Jepang diserahkan, Namun, baik warga maupun pasukan bersenjata menolak untuk mematuhi perintah dalam ultimatum tersebut. Dalam waktu tujuh jam, sekitar 200.000 warga Bandung secara kolektif membakar rumah dan harta benda mereka. Setelah itu, mereka mengungsi ke daerah selatan Bandung. Masyarakat Bandung saat itu membakar gedung-gedung penting, meledakkan bangunan-bangunan besar, termasuk gudang mesiu yang diledakkan oleh Mohammad Toha, dan merusak instalasi militer.
+</p>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  
+ 
+  </section >
+  <!-- jumbotron akhir -->
+
+
+
+
+  <!-- hikmah -->
+  <section id="Hikmah">
+    <div class="container">
+     <div class="row text-center mb-3">
+      <div class="col">
+        <br>
+        <br>
+        <br>
+      <h2> HIKMAH </h2>
+    </div>
+  </div>
+  <div class ="row justify-content-center ">
+    <div class ="col-4">
+      <p> pentingnya menjaga persatuan dan kesatuan bangsa Indonesia yang berdasarkan Pancasila dan UUD 1945. Pancasila adalah ideologi yang mampu mengakomodasi keberagaman bangsa Indonesia tanpa mengesampingkan nilai-nilai keislaman.
+      </p>
+     </div>
+     <div class ="col-4">
+     <p> memperjuangkan kepentingan bersama, membangun ekonomi, sosial budaya, menegakan kedaulatan negara, menentang rasisme dan menguatkan kedudukan perdamaian dunia.</p>
+    </div>
+   </div>
+  </div>
+
+ </section>
+ <br>
+ <br>
+  <!-- akhir hikmah -->
+ 
+
+
+
+  <!-- Dokumentasi -->
+  <section id="Dokumentasi">
+   <div class="container">
+    <div class="row text-center">
+     <div class="col">
+      <br>
+      <br>
+      <br>
+      <h2>DOKUMENTASI</h2>
+      </div>
+
+        <div class="row">
+          <div class="col-md-4 mb-3">
+             <div class="card" >
+                 <img src="image/dd.jpg" class="card-img-top" alt="dd.jpg">
+                 <div class="card-body">
+                 <h5 class="card-title">KANTOR GUBERNUR</h5>
+                 <p class="card-text">dikenal dengan sebutan Kantor Gubernur karena sebagai pusat kegiatan Pemerintah Provinsi Jawa Barat, yang sebelumnya Pemerintahaan Provinsi Jawa Barat menempati Gedung Kerta Mukti di Jalan Braga Bandung.</p>
+             </div>
+           </div>
+         </div>
+         <div class="col-md-4 mb-3">
+             <div class="card" >
+                 <img src="image/download66.jpg" class="card-img-top" alt="download66.jpg">
+                 <div class="card-body">
+                 <h5 class="card-title">MONUMEN BANDUNG LAUTAN API</h5>
+                 <p class="card-text">Monumen ini dibangun untuk memperingati peristiwa Bandung Lautan Api, dimana terjadi pembumihangusan Bandung Selatan yang dipimpin oleh Muhammad Toha</p>
+             </div>
+           </div>
+         </div>
+         <div class="col-md-4 mb-3">
+             <div class="card" >
+                 <img src="image/download44.jpg" class="card-img-top" alt="download44.jpg">
+                 <div class="card-body">
+                 <h5 class="card-title">Card title</h5>
+                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+             </div>
+           </div>
+         </div>
+         <div class="col-md-4 mb-3">
+             <div class="card" >
+                 <img src="image/download33.jpg" class="card-img-top" alt="download33.jpg">
+                 <div class="card-body">
+                 <h5 class="card-title">ILUSTRASI TRAGEDI</h5>
+                 <p class="card-text">Ilustrasi hangusnya bandung yang di pimpin Moh.Toha</p>
+             </div>
+           </div>
+         </div>
+         <div class="col-md-4 mb-3">
+             <div class="card" >
+                 <img src="image/download22.jpg" class="card-img-top" alt="download22.jpg">
+                 <div class="card-body">
+                 <h5 class="card-title">MOMEN BANDUNG LAUTAN API</h5>
+                 <p class="card-text">Huru hara yang terjadi pada saat tragedi Bandung Lautan Api</p>
+             </div>
+           </div>
+         </div>
+         <div class="col-md-4 mb-3">
+             <div class="card" >
+                 <img src="image/download11.jpg" class="card-img-top" alt="download11.jpg">
+                 <div class="card-body">
+                 <h5 class="card-title">Card title</h5>
+                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+             </div>
+           </div>
+         </div>
+       </div>
+     </div>
+  </div>
+ </section>
+ <br>
+ <br>
+  <!-- Akhir Dokumentasi -->
+
+
+
+
+
+
+
+  <!-- Tokoh -->
+  <section id="Tokoh">
+   <div class="container">
+    <div class="row text-center">
+     <div class="col">
+      <br>
+      <br>
+      <br>
+      <h2>PARA TOKOH PAHLAWAN</h2>
+      </div>
+
+        <div class="row">
+          <div class="col-md-4 mb-3">
+             <div class="card" >
+                 <img src="image/atdji.jpg" class="card-img-top" alt="atdji.jpg">
+                 <div class="card-body">
+                 <h5 class="card-title">Atje Bastaman</h5>
+                 <p class="card-text">Atje Bastaman, seorang wartawan muda, juga memiliki peran penting dalam peristiwa Bandung Lautan Api. Ia menjadi saksi langsung proses pembakaran Bandung yang ia saksikan dari bukit Gunung Leutik di sekitar Pameungpeuk, Garut.</p>
+             </div>
+           </div>
+         </div>
+         <div class="col-md-4 mb-3">
+             <div class="card" >
+                 <img src="image/toha2.jpg" class="card-img-top" alt="toha2.jpg">
+                 <div class="card-body">
+                 <h5 class="card-title">Mohammad Toha</h5>
+                 <p class="card-text">Mohammad Toha, tokoh yang signifikan dalam peristiwa Bandung Lautan Api, terlibat dalam usaha meledakkan gudang mesiu dengan tujuan menghancurkan Bandung. Namun, dalam menjalankan tugas tersebut, ia ikut gugur akibat ledakan yang terjadi.</p>
+             </div>
+            </div>
+         </div>
+         <div class="col-md-4 mb-3">
+             <div class="card" >
+                 <img src="image/sutan_sjahrir.jpg" class="card-img-top" alt="sutan_sjahrir.jpg">
+                 <div class="card-body">
+                 <h5 class="card-title">Sutan Sjahrir</h5>
+                 <p class="card-text">Sutan Sjahrir, yang pada saat peristiwa Bandung Lautan Api menjabat sebagai Perdana Menteri Republik Indonesia, juga merupakan tokoh penting dalam peristiwa tersebut.</p>
+             </div>
+           </div>
+         </div>
+         <div class="col-md-4 mb-3">
+             <div class="card" >
+                 <img src="image/ismailmarzuki.jpg" class="card-img-top" alt="ismailmarzuki.jpg">
+                 <div class="card-body">
+                 <h5 class="card-title">Ismail Marzuki</h5>
+                 <p class="card-text">Selain dikenal sebagai pencipta lagu "Halo-halo Bandung", Ismail Marzuki juga merupakan salah satu orang yang mengungsi selama peristiwa Bandung Lautan Api. Bersama dengan istrinya, ia mengungsi untuk menghindari pendudukan tentara Inggris dan Belanda di Bandung.</p>
+             </div>
+           </div>
+         </div>
+         <div class="col-md-4 mb-3">
+             <div class="card" >
+                 <img src="image/mayorrukana.jpg" class="card-img-top" alt="mayorrukana.jpg">
+                 <div class="card-body">
+                 <h5 class="card-title">Mayor Rukana</h5>
+                 <p class="card-text">Awalnya, Inggris hanya meminta masyarakat Bandung untuk mengosongkan kota. Namun, Mayor Rukana merasa tidak rela melihat kotanya digunakan oleh pihak musuh, sehingga ia mengusulkan pembakaran Kota Bandung.</p>
+             </div>
+           </div>
+         </div>
+         <div class="col-md-4 mb-3">
+             <div class="card" >
+                 <img src="image/nasution.jpg" class="card-img-top" alt="nasution.jpg">
+                 <div class="card-body">
+                 <h5 class="card-title">Abdul Haris Nasution </h5>                              
+                 <p class="card-text">Kolonel Abdul Haris Nasution, yang juga dikenal sebagai AH Nasution, adalah Komandan Divisi III Tentara Republik Indonesia (TRI) yang memerintahkan evakuasi masyarakat Bandung sebagai respons terhadap serangan Sekutu.
+                 </p>
+             </div>
+           </div>
+         </div>
+       </div>
+     </div>
+  </div>
+ </section>
+ <br>
+ <br>
+  <!-- Akhir Tokoh -->
+
+
+
+<!-- awal form -->
+<div class="container">
+<h2>Formulir Buku Tamu</h2>
+<br>
+<form id="Pesan">
+  <table>
+    <tr>
+      <td>Nama</td>
+      <td><input type="text" name="nama"></td>
+    </tr>
+
+    <tr>
+      <td>Alamat</td>
+      <td><input type="text" name="alamat"></td>
+    </tr>
+
+    <tr>
+      <td>Email</td>
+      <td><input type="text" name="email"></td>
+    </tr>
+
+    <tr>
+      <td>Pesan</td>
+      <td><textarea name="pesan" id="" cols="50" rows="4"></textarea></td> 
+    </tr>
+  </table>
+  <button type="submit">kirim</button>
+  <button type="submit">batal</button>
+</form>
+</div>
+
+
+<!-- akhir form -->
+
+
+
+
+
+
+  <!-- finishing -->
+<footer class="bg-light text white text-center p-3">
+  <p>
+    Created by <a href="https://www.instagram.com/daafirh/" class="text- white fw-bold">Darfian Fakhri Usmawan</a>
+  </p>
+</footer>
+  <!-- akhir finishing -->
+
+  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+  </body>
+</html>
